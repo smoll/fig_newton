2f5e3cc264db9e60527f467bcd96b3618ca1fb26
smoll <smollah@theorchard.com>
2015-01-23 12:15:18 -0500

    Allow Booleans as permissible return values from YML

--- lib/fig_newton/missing.rb
+++ lib/fig_newton/missing.rb
@@ -7,9 +7,9 @@ module FigNewton
       read_file unless @yml
       m = args.first
       value = @yml[m.to_s]
-      value = args[1] unless value
-      value = block.call(m.to_s) unless value or block.nil?
-      super unless value
+      value = args[1] unless value || type_bool?(value)
+      value = block.call(m.to_s) unless value || block.nil? || type_bool?(value)
+      super unless value || type_bool?(value)
       value = FigNewton::Node.new(value) unless type_known? value
       value
     end
@@ -21,14 +21,18 @@ module FigNewton
         hostname = Socket.gethostname
         hostfile = "#{yml_directory}/#{hostname}.yml"
         @yml = YAML.load_file hostfile if File.exist? hostfile
-      end 
+      end
       FigNewton.load('default.yml') if @yml.nil?
     end
 
     private
-    
+
     def type_known?(value)
-       value.kind_of? String or value.kind_of? Integer
+      value.is_a?(String) || value.is_a?(Integer) || type_bool?(value)
+    end
+
+    def type_bool?(value)
+      value.is_a?(TrueClass) || value.is_a?(FalseClass)
     end
   end
 end
